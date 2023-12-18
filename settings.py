4585838b65870766fe0ab1a76640ebabbb2359d0
ratchek <git@ratchek.com>
2023-12-18 16:34:41 -0500

    Add basic site structure

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 TEMPLATE_DIR = "templates"
-STATIC_DIR = "static"
+STATIC_DIR = "assets"
 SRC_DIR = "src"
 OUTPUT_DIR = "output"
-TEMPLATE_LIST = ["index.html"]
+TEMPLATE_LIST = ["index.html", "onecolumn.html", "threecolumn.html", "twocolumn1.html", "twocolumn2.html"]
