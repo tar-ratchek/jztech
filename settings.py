5590a76e73e01105305b6a511e579ec5239614b3
ratchek <git@ratchek.com>
2023-12-28 15:16:51 -0500

    Add relevant pages

--- settings.py
+++ settings.py
@@ -2,4 +2,13 @@ TEMPLATE_DIR = "templates"
 STATIC_DIR = "assets"
 SRC_DIR = "src"
 OUTPUT_DIR = "output"
-TEMPLATE_LIST = ["index.html", "onecolumn.html", "threecolumn.html", "twocolumn1.html", "twocolumn2.html"]
+TEMPLATE_LIST = [
+    "index.html",
+    "about.html",
+    "product.html",
+    "contact.html",
+    "onecolumn.html",
+    "threecolumn.html",
+    "twocolumn1.html",
+    "twocolumn2.html",
+]
